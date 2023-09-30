e271760b8e3be50a4a4f78a111605866279bc55b
Corwin <gmail@corwincheung.com>
2023-09-30 11:25:02 -0400

    Done w Day7

--- Day7.py
+++ Day7.py
@@ -13,11 +13,11 @@ def non_optimized(num):
 
     return recurse(num)
 
-#like in the Fibonacci sequence, split the string into the number of ways you can split
-#taking off the last letter like floor of dividing by 10, with the edge case of 0 and flooring
-#of dividing by 100 if the last two could be a letter in the alphabet, use the base case of 0 return 1 case.
-#The time complexity of this solution is roughly O(2^n), because for each additional digit you 
-#add onto the string, you must consider two times as many letter decoding combinations
+# like in the Fibonacci sequence, split the string into the number of ways you can split
+# taking off the last letter like floor of dividing by 10, with the edge case of 0 and flooring
+# of dividing by 100 if the last two could be a letter in the alphabet, use the base case of 0 return 1 case.
+# The time complexity of this solution is roughly O(2^n), because for each additional digit you
+# add onto the string, you must consider two times as many letter decoding combinations
 
 
 def optimized(num):
@@ -38,17 +38,17 @@ def optimized(num):
             return ways
     return recurse(num)
 
-#use the same logic but each time you complete a substring n from recurse, store it to a 
-#dictionary called substrings and then check if you have a memoized version of the substring
-#to save on time complexity
-#The time complexity of this function is worst case O(n) since each of the substrings are stored 
-# so in the worst case scenario, the function runs through all of the substirngs. If it reaches an 
-#already computed substring it returns and does not have to run more computations.
+# use the same logic but each time you complete a substring n from recurse, store it to a
+# dictionary called substrings and then check if you have a memoized version of the substring
+# to save on time complexity
+# The time complexity of this function is worst case O(n) since each of the substrings are stored
+# so in the worst case scenario, the function runs through all of the substirngs. If it reaches an
+# already computed substring it returns and does not have to run more computations.
 
 
 def main():
     # calculates ways to decode
-    num = 11111
+    num = 102111123123112312312312113111231
     print(non_optimized(num))
 
     print(optimized(num))
